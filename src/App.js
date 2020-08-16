a602dca94dcb1f6910bdd7d9c6c3f8d38c3126ad
JinwonPark <58999707+Dnopark@users.noreply.github.com>
2020-08-16 20:40:00 +0900

    init fokin-weather

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello world!
+          hello dno!
         </p>
         <a
           className="App-link"
